5a45f38ef3e27812b723db112c6086f41969b4dc
Tristan <tschartman41@gmail.com>
2022-03-28 12:51:04 -0400

    backup
    Escape breaks everything

--- conlib.py
+++ conlib.py
@@ -4,9 +4,6 @@ import tty
 import termios
 from typing import Tuple, Union, Callable, Any
 
-log : str = ""
-isnllog : list[tuple[bool, str]] = []
-
 def set_raw_decorator (func : Callable[[Any], Any]) -> Callable[[Any], Any]:
     """
     decorates a function by setting the terminal to raw mode, then reverting it after the decorated function has completed
@@ -20,82 +17,82 @@ def set_raw_decorator (func : Callable[[Any], Any]) -> Callable[[Any], Any]:
             res = func(*args, **kwargs)
         finally:
             termios.tcsetattr(fileno, termios.TCSADRAIN, old_settings)
+            print("\x1b[1G", end="")
         return res
     return ret
 
-class Console ():
-    @set_raw_decorator
-    def getchar (show_entered : bool = True) -> str:
-        """
-        reads a character from sys.stdin
-        """
-        c : str = sys.stdin.read(1)
+def _readin_to_char (char : str) -> str:
+    """
+    reads sys.stdin until "char" is reached, note that this shouldn't be used to read actual user input
+    """
+    f : str = ""
+    while True:
+        f += sys.stdin.read(1)
+        if f[-1] == char:
+            break
+    return f
+
+@set_raw_decorator
+def getcurpos () -> Tuple[int, int]:
+    """
+    gets the cursor's current position in the format (x, y)
+    """
+    print("\x1b[6n", sep="", end="", flush=True)
+    text : str = _readin_to_char("R")
+    x : int = int(text[2:text.index(";")])
+    y : int = int(text[text.index(";")+1:text.index("R")])
+    return (x, y)
+
+@set_raw_decorator
+def getchar (show_entered : bool = True) -> str:
+    """
+    reads a character from sys.stdin
+    """
+    c : str = sys.stdin.read(1)
+    if c == "\x1b":
+        c2 : str = sys.stdin.read(1)
+        while not c2.isalpha():
+            c += c2
+            c2 = sys.stdin.read(1)
+        c += c2
+    if show_entered:
         print(c, sep="", end="", flush=True)
-        return c
+    return c
 
-    def _readin_to_char (char : str) -> str:
-        """
-        reads sys.stdin until "char" is reached, note that this shouldn't be used to read actual user input
-        """
-        f : str = ""
-        while True:
-            f += sys.stdin.read(1)
-            if f[-1] == char:
-                break
-        return f
+def get_size () -> Tuple[int, int]:
+    """
+    returns the current terminal size in the format (columns, rows)
+    """
+    sizing = os.get_terminal_size()
+    return (sizing.columns, sizing.lines)
 
-    @set_raw_decorator
-    def getcurpos () -> Tuple[int, int]:
-        """
-        gets the cursor's current position in the format (x, y)
-        """
-        print("\x1b[6n", sep="", end="", flush=True)
-        text : str = Console._readin_to_char("R")
-        x : int = int(text[2:text.index(";")])
-        y : int = int(text[text.index(";")+1:text.index("R")])
-        return (x, y)
+def getchars (n : int = 1, show_entered_chars : bool = True) -> str:
+    """
+    gets the requested number of characters
+    """
+    chars : str= ""
+    for i in range(n):
+        chars += getchar(show_entered_chars)
+    return chars
 
-    def get_size () -> Tuple[int, int]:
-        """
-        returns the current terminal size in the format (columns, rows)
-        """
-        sizing = os.get_terminal_size()
-        return (sizing.columns, sizing.rows)
+@set_raw_decorator
+def readin (prompt : str = "", *, show_entered_chars : bool = True, replace_chars_with : Union[str, None] = None) -> str:
+    """
+    returns user input
+    """
+    print(prompt, sep="", end="", flush=True)
+    f : str = ""
+    global log
+    while True:
+        c : str = sys.stdin.read(1).replace("\r", "\n")
+        if show_entered_chars:
+            print(c if replace_chars_with == None or c == "\n" else replace_chars_with, sep="", end="", flush=True)
+        if c in "\x03\x04":
+            raise KeyboardInterrupt()
+        if c == "\n":
+            break
+        f += c
+    return f
 
-    def getchars (n : int = 1, show_entered_chars : bool = True) -> str:
-        """
-        gets the requested number of characters
-        """
-        chars : str= ""
-        for i in range(n):
-            chars += Console.getchar(show_entered_chars)
-        return chars
-    
-    @set_raw_decorator
-    def input (prompt : str = "", show_entered_chars : bool = True, replace_chars_with : Union[str, None] = None) -> str:
-        """
-        returns user input
-        """
-        f : str = ""
-        global log
-        while True:
-            c : str = sys.stdin.read(1)
-            if c == "\r":
-                c = "\n"
-            log += c
-            if show_entered_chars:
-                print(c if replace_chars_with == None or c == "\n" else replace_chars_with, sep="", end="", flush=True)
-            if c in "\x03\x04":
-                raise KeyboardInterrupt()
-            isnllog.append((c == "\n", hex(ord(c))))
-            if c == "\n":
-                break
-            f += c
-        print("\x1b[2K", end="", flush=True)
-        return f
-    
-    def log () -> str:
-        return log
-    
-    def nlog () -> list[str]:
-        return isnllog
+def clear ():
+    print("\x1bc", sep="", end="", flush=True)
--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-from conlib import Console as con
+from conlib import *
 
-try:
-    # inp : str = con.getchar(2)
-    inp : str = con.input(replace_chars_with="*")
-    print("xx", inp)
-    print(hex(ord(inp[0])), len(inp))
-finally:
-    pass
-    print("LOG")
-    # print(con.log())
-    print(*con.nlog(), sep="\n")
+class KEYS ():
+    ArrowUp = "\x1b[A"
+    ArrowDown = "\x1b[B"
+    ArrowRight = "\x1b[C"
+    ArrowLeft = "\x1b[D"
+    BackSpace = "\x08"
+    Delete = "\x7f"
+    Enter = "\x0d"
+    Tab = "\x09"
+    Escape = "\x1b"
+
+class Build ():
+
+    def __init__ (self) -> None:
+        self.user_pos : list[int, int] = [0, 0]
+        self.box_pos : list[int, int] = [0, 0]
+        self.screen_size : tuple[int, int] = get_size()
+
+    def key_event (self, key : str) -> None:
+        if key == KEYS.ArrowRight:
+            print("arrow right")
+        elif key == KEYS.ArrowLeft:
+            print("arrow left")
+        elif key == KEYS.ArrowUp:
+            print("arrow up")
+        elif key == KEYS.ArrowDown:
+            print("arrow down")
+        elif key == KEYS.Tab:
+            print("tab")
+        elif key in (KEYS.BackSpace, KEYS.Delete):
+            print("backspace")
+        elif key == KEYS.Enter:
+            print("enter")
+        elif key == KEYS.Escape:
+            print("escape")
+        else:
+            print(key, hex(ord(key)))
+    
+    def run (self) -> None:
+        while True:
+            key = getchar(False)
+            if key == "\x03":
+                break
+            self.key_event(key)
+
+Build().run()
